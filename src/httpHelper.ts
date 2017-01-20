7d9d4984e910cb6e9671750078f6ba2c13638352
Greg Lincoln <greg@greglincoln.com>
2017-01-19 23:30:54 -0500

    Detect the correct port for the autocomplete service.

--- src/httpHelper.ts
+++ src/httpHelper.ts
@@ -1,6 +1,9 @@
-import {CompletionItem} from 'vscode';
+import {CompletionItem, workspace, window } from 'vscode';
 import * as http from 'http';
 import * as request from 'request';
+import * as fs from 'fs';
+import * as path from 'path';
+const md5 = require('md5');
 
 interface CompletionRequest {
     path: string;
@@ -13,7 +16,51 @@ interface CompletionResponse {
     suggestions: string[];
 }
 
+function getServersListPath() {
+    if (process.env.APPDATA) {
+        return process.env.APPDATA + "/Godot/.autocomplete-servers.json" 
+    }
+    else if (process.env.HOME) {
+        return process.env.HOME + "/.godot/.autocomplete-servers.json";
+  }
+}
+
 export class CompletionService {
+    private static portForProject;
+
+    private static refreshProjectPort(force: boolean = false): Promise<string> {
+        if (force) { CompletionService.portForProject = null };
+
+        return new Promise((resolve, reject) => {
+            if (CompletionService.portForProject) {
+                resolve(CompletionService.portForProject);
+                return;
+            }
+            
+            workspace.findFiles("engine.cfg", "").then(ec => {
+                if (ec.length != 1) {
+                    console.log(`Found ${ec.length} engine.cfg files. Expected 1.`);
+                    reject(null);
+                }
+                const projectPath = path.dirname(ec[0].fsPath);
+                console.log(`Using ${projectPath} as project path.`);
+
+                const serversListPath = getServersListPath();
+                if (fs.existsSync(serversListPath)) {
+                    const servers = JSON.parse(fs.readFileSync(serversListPath, 'utf8'));
+                    const port = servers[md5(projectPath)];
+                    if (port) {
+                        CompletionService.portForProject = port;
+                        resolve(port);
+                    } else {
+                        console.log("Could not find a match for this project.");
+                        reject(null);
+                    }
+                }
+            })
+        });
+    }
+
     static getCompletions(request: CompletionRequest): Promise<CompletionItem[]> {
         return new Promise(resolve => {
             CompletionService.post(request)
@@ -22,10 +69,15 @@ export class CompletionService {
     }
 
     private static post(body: Object): Promise<CompletionResponse> {
-        return new Promise((resolve, reject) => {
-            request({ uri: 'http://localhost:' + '6070', method: 'POST', json: body }, (error, response, body) => {
-                if (error) { reject(error); }
-                resolve(body);
+        return new Promise((resolve, reject) => {                        
+            CompletionService.refreshProjectPort().then(port => {
+                request({ uri: 'http://localhost:' + port, method: 'POST', json: body }, (error, response, body) => {
+                    if (error) {
+                        CompletionService.refreshProjectPort(true).then(port => CompletionService.portForProject = port);
+                        reject(error);
+                    }
+                    resolve(body);
+                });
             });
         });
     }
